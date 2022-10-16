da3f327fddd51f65d255e96c71cd8fde71305a0b
Orchit07 <orchitiadi@gmail.com>
2022-10-17 06:13:23 +0700

    chore(config): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,13 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                "alfa-slab-one": ["alfa-slab-one"],
-                segoe: ["segoe"],
-                "segoe-bold": ["segoe-bold"]
+                "alfa-slab-one": ["alfa-slab-one", "sans-serif"],
+                segoe: ["segoe", "sans-serif"],
+                "segoe-bold": ["segoe-bold", "sans-serif"],
+                garet: ["garet", "sans-serif"],
+                "garet-bold": ["garet-bold", "sans-serif"]
             }
         }
     },
-    plugins: [
-        require("@headlessui/tailwindcss"),
-        require("@tailwindcss/typography"),
-        require("@tailwindcss/forms")({ strategy: "class" })
-    ]
+    plugins: [require("@headlessui/tailwindcss")]
 };
