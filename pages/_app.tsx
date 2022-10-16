f4bb076ffd0fdb6a4b52f60f61a3d5080554103c
Orchit07 <orchitiadi@gmail.com>
2022-10-17 06:18:31 +0700

    chore(_app): remove unnecessary tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import MyHead from "@/components/MyHead";
 import MyNavbar from "@/components/MyNavbar";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
@@ -13,9 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <ThemeProvider enableSystem attribute="class">
                 <MyNavbar />
                 <NextNProgress />
-                <SessionProvider>
-                    <Component {...pageProps} />
-                </SessionProvider>
+                <Component {...pageProps} />
             </ThemeProvider>
         </>
     );
