ec2bdaa1cc6b8987637595366fb96abc46ab945c
Orchit07 <orchitiadi@gmail.com>
2022-10-18 16:49:09 +0700

    chore(config): update next js config

--- next.config.js
+++ next.config.js
@@ -4,17 +4,7 @@
 module.exports = {
     reactStrictMode: true,
     images: { domains: ["cdn.discordapp.com"] },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false;
-            config.resolve.fallback.child_process = false;
-            config.resolve.fallback.request = false;
-            config.resolve.fallback.net = false;
-            config.resolve.fallback.worker_threads = false;
-            config.resolve.fallback.tls = false;
-        }
-        return config;
-    },
+    swcMinify: true,
     async redirects() {
         return [
             {
