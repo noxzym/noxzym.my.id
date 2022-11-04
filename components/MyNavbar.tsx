f1d8192ba9accb1bceb7a407dcabf6e257c64dd6
noxzym <orchitiadi@gmail.com>
2022-11-05 06:34:04 +0700

    chore(MyNavbar): use new next/Link feature

--- components/MyNavbar.tsx
+++ components/MyNavbar.tsx
@@ -31,16 +31,16 @@ function GetNavigationElement(pathname?: string) {
             key={i}
             className="flex w-full text-center capitalize text-[#222831] dark:text-[#DDDDDD] lg:block lg:w-auto lg:text-left"
         >
-            <Link href={item.path} className="cursor-pointer" rel="noreferrer">
-                <a
-                    className={`w-full rounded py-2 lg:w-auto lg:px-4`.concat(
-                        item.name === "blog"
-                            ? " rounded-full bg-[#222831] text-[#DDDDDD] dark:bg-[#DDDDDD] dark:text-[#111111]"
-                            : " hover:bg-[#C7C7C7] dark:hover:bg-[#171717] lg:rounded-none hover:lg:border-b-2 hover:lg:border-[#222831] hover:lg:bg-transparent dark:hover:lg:border-[#DDDDDD] dark:hover:lg:bg-transparent"
-                    )}
-                >
-                    {item.name}
-                </a>
+            <Link
+                href={item.path}
+                className={`w-full cursor-pointer rounded py-2 lg:w-auto lg:px-4`.concat(
+                    item.name === "blog"
+                        ? " rounded-full bg-[#222831] text-[#DDDDDD] dark:bg-[#DDDDDD] dark:text-[#111111]"
+                        : " hover:bg-[#C7C7C7] dark:hover:bg-[#171717] lg:rounded-none hover:lg:border-b-2 hover:lg:border-[#222831] hover:lg:bg-transparent dark:hover:lg:border-[#DDDDDD] dark:hover:lg:bg-transparent"
+                )}
+                rel="noreferrer"
+            >
+                {item.name}
             </Link>
         </div>
     ));
