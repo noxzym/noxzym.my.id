4b1f2c79a9cf043461a34992917051b11ccb41f5
noxzym <orchitiadi@gmail.com>
2022-12-27 13:55:06 +0700

    chore(pages/_app): update app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import MyHead from "@/components/MyHead";
+import { ProgressBar } from "@/components/ProgressBar";
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import "../styles/index.scss";
 
-const ProgressBar = dynamic(
-    () => {
-        return import("@/components/ProgressBar");
-    },
-    { ssr: false }
-);
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
