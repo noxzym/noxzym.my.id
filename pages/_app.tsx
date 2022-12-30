e79f6bbc46cc89639f1318b0dc85f9f0b6722dd1
noxzym <orchitiadi@gmail.com>
2022-12-30 14:58:13 +0700

    chore(deps): add @vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import MyHead from "@/components/MyHead";
 import { ProgressBar } from "@/components/ProgressBar";
+import { Analytics } from "@vercel/analytics/react";
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
@@ -10,9 +11,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <MyHead />
             <ThemeProvider enableSystem attribute="class">
-                <AnimatePresence mode="wait" initial={false}>
+                <AnimatePresence initial={false}>
                     <ProgressBar />
                     <Component {...pageProps} />
+                    <Analytics />
                 </AnimatePresence>
             </ThemeProvider>
         </>
