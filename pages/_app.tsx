b1c655cc929e7a60f92317576e2faf723079303f
noxzym <67855694+noxzym@users.noreply.github.com>
2023-05-06 16:53:00 +0700

    chore: add application metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                     }
                 `}
             </style>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="application-name" content="Noxzym" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta
+                name="apple-mobile-web-app-status-bar-style"
+                content="default"
+            />
+            <meta name="apple-mobile-web-app-title" content="Noxzym" />
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="theme-color" content="#DDDDDD" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+            />
             <DefaultSeo
                 description="noxzym's personal website"
                 additionalLinkTags={[
