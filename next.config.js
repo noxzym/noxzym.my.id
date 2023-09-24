2579c4ba8acdbc3e78840ed6baad8a978b9d0c4b
noxzym <orchitiadi@gmail.com>
2023-09-24 21:41:32 +0700

    chore(nextjs): use next-pwa and add redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,41 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const WithPWA = require("next-pwa");
 
-module.exports = nextConfig;
+module.exports = WithPWA({
+    dest: "public",
+    register: process.env.NODE_ENV === "production" ? true : false
+})({
+    async redirects() {
+        return [
+            {
+                source: "/discord",
+                destination: "https://discord.com/users/243728573624614912",
+                permanent: true
+            },
+            {
+                source: "/github",
+                destination: "https://github.com/noxzym",
+                permanent: true
+            },
+            {
+                source: "/spotify",
+                destination: "https://open.spotify.com/user/21kwa7qnjne3q3w3l3x2jnahi",
+                permanent: true
+            },
+            {
+                source: "/instagram",
+                destination: "https://www.instagram.com/orchit07",
+                permanent: true
+            },
+            {
+                source: "/twitter",
+                destination: "https://twitter.com/noxzym",
+                permanent: true
+            },
+            {
+                source: "/linkedin",
+                destination: "https://www.linkedin.com/in/orchitiadi",
+                permanent: true
+            }
+        ];
+    }
+});
