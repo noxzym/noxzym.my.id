ef759ccc3abbc0018371ef2a4d709dd3c1ce0d32
noxzym <orchitiadi@gmail.com>
2023-09-24 21:47:52 +0700

    feat(components): add my avatar

---
+++ src/components/MyAvatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useLanyard } from "@/hooks/useLanyard";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export function MyAvatar() {
+    const { data } = useLanyard();
+
+    return (
+        <Avatar className="h-full w-full rounded-lg">
+            <AvatarImage
+                src={`https://cdn.discordapp.com/avatars/${data?.data.discord_user.id}/${data?.data.discord_user.avatar}.png?size=4096`}
+                alt="@noxzym"
+            />
+            <AvatarFallback />
+        </Avatar>
+    );
+}
