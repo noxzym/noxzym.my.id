5e37b76bce210f31832aaff816e4d696a44e9a5f
noxzym <orchitiadi@gmail.com>
2023-09-25 18:58:03 +0700

    chore(useLanyard): add argument type

--- src/hooks/useLanyard.ts
+++ src/hooks/useLanyard.ts
@@ -2,7 +2,7 @@ import { ILanyard } from "@/types";
 import useSWR from "swr";
 
 export const useLanyard = () => {
-    const { data, error } = useSWR<ILanyard>("/api/lanyard", args =>
+    const { data, error } = useSWR<ILanyard>("/api/lanyard", (args: string) =>
         fetch(args).then(res => res.json())
     );
 
