444a9842133bbd816fb3402bd5f2501a42284d73
noxzym <orchitiadi@gmail.com>
2023-10-18 15:00:40 +0700

    chore(BlogList): use framer-motion

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { IArticle } from "@/types";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Icons } from "../utils/Icons";
 
 export const BlogList = ({ pages }: { pages: IArticle[] }) => {
     const [state, setState] = useState<string>("");
@@ -26,43 +27,63 @@ export const BlogList = ({ pages }: { pages: IArticle[] }) => {
                         .includes(state.toLowerCase())
                 )
                 .map((page, i) => (
-                    <div key={i} className="grid w-full gap-3 md:grid-cols-3">
-                        <Link href={`/blog/${page.slug}`}>
+                    <motion.div
+                        key={i}
+                        variants={{
+                            initial: {
+                                opacity: 0,
+                                y: 50
+                            },
+                            animate: {
+                                opacity: 1,
+                                y: 0,
+                                transition: {
+                                    delay: i * 0.09
+                                }
+                            }
+                        }}
+                        animate="animate"
+                        initial="initial"
+                    >
+                        <Link
+                            href={`/blog/${page.slug}`}
+                            className="grid w-full gap-3 transition-transform hover:scale-105 md:grid-cols-3"
+                        >
                             <Avatar className="aspect-video h-auto w-full rounded-lg">
                                 <AvatarImage src={page.image} className="object-cover" />
                                 <AvatarFallback />
                             </Avatar>
-                        </Link>
-                        <div className="flex w-full flex-col gap-3 self-center md:col-span-2">
-                            <div className="flex w-full flex-col gap-1">
-                                <Link
-                                    href={`/blog/${page.slug}`}
-                                    className="text-inherit no-underline"
-                                >
-                                    <p className="font-sans text-xl font-bold dark:text-white/80">
+                            <div className="flex w-full flex-col gap-3 dark:text-white/70 md:col-span-2">
+                                <div className="flex w-full flex-col gap-2">
+                                    <p className="text-xl font-bold dark:text-white/80">
                                         {page.title}
                                     </p>
-                                </Link>
-                                <p className="font-sans text-xs font-semibold text-[#909090] dark:text-white/50">
-                                    {page.publishedTime}
-                                </p>
-                                <p className="font-sans text-sm font-medium dark:text-white/50">
-                                    {page.description}
-                                </p>
+                                    <div className="flex gap-2 text-xs font-medium">
+                                        <div className="flex items-center gap-1">
+                                            <Icons icon="calendar" className="h-4 w-4" />
+                                            <p>{page.publishedTime}</p>
+                                        </div>
+                                        •
+                                        <div className="flex items-center gap-1">
+                                            <Icons icon="time" className="h-4 w-4" />
+                                            <p>{page.readTime}</p>
+                                        </div>
+                                    </div>
+                                    <p className="text-sm">{page.description}</p>
+                                </div>
+                                <div className="flex w-full gap-2">
+                                    {page.tags.map((tag, i) => (
+                                        <div
+                                            key={i}
+                                            className="rounded bg-black/10 px-4 py-2 dark:bg-white/10"
+                                        >
+                                            <p className="text-xs font-medium">{tag}</p>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                            <div className="flex w-full flex-wrap gap-2">
-                                {page.tags.map((tag, i) => (
-                                    <Button
-                                        key={i}
-                                        color="inherit"
-                                        className="bg-[#C9C9C9] px-3 py-1 font-sans text-xs font-semibold normal-case"
-                                    >
-                                        {tag}
-                                    </Button>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+                        </Link>
+                    </motion.div>
                 ))}
         </div>
     );
