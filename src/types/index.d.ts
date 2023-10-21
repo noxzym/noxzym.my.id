0c9c750b496e40efd2e8f560b04884e09f76c1ff
noxzym <orchitiadi@gmail.com>
2023-10-21 21:02:33 +0700

    chore(typings): add spotify track type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -74,3 +74,42 @@ export type PageProps<T extends string | string[]> = {
         slug: T;
     };
 };
+
+type SongImage = {
+    url: string;
+};
+
+type SongArtist = {
+    name: string;
+};
+
+type SongAlbum = {
+    name: string;
+    artists: Array<SongArtist>;
+    images: Array<SongImage>;
+};
+
+type ExternalUrl = {
+    spotify: string;
+};
+
+type SongItem = {
+    name: string;
+    album: SongAlbum;
+    external_urls: ExternalUrl;
+};
+
+export type GetNowPlayingResponse = {
+    is_playing: boolean;
+    item: SongItem;
+    currently_playing_type: string;
+};
+
+export type GetNowPlayingTransformed = {
+    isPlaying?: boolean;
+    album?: string;
+    artist?: string;
+    title?: string;
+    url?: string;
+    artworkUrl?: string;
+};
