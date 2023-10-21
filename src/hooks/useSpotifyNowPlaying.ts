a0abbc8dcdd5f68f1294ef564c7984708631afe7
noxzym <orchitiadi@gmail.com>
2023-10-21 21:50:15 +0700

    chore(useSpotifyNowPlaying): disable caching

--- src/hooks/useSpotifyNowPlaying.ts
+++ src/hooks/useSpotifyNowPlaying.ts
@@ -3,9 +3,16 @@ import { GetNowPlayingTransformed } from "@/types";
 import useSWR from "swr";
 
 export const useSpotifyNowplaying = () => {
-    const { data, error } = useSWR<GetNowPlayingTransformed>("/api/now-playing", fetcher, {
-        refreshInterval: 10000
-    });
+    const { data, error } = useSWR<GetNowPlayingTransformed>(
+        "/api/now-playing",
+        (args: string) =>
+            fetcher<GetNowPlayingTransformed>(args, {
+                cache: "no-cache"
+            }),
+        {
+            refreshInterval: 10000
+        }
+    );
 
     return {
         data,
