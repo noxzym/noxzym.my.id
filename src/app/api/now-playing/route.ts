91471e941e2303ec3dce9c9b7a91515e054be458
noxzym <orchitiadi@gmail.com>
2023-10-22 13:21:55 +0700

    chore(api\now-playing): use revalidate config

--- src/app/api/now-playing/route.ts
+++ src/app/api/now-playing/route.ts
@@ -3,13 +3,15 @@ import { getAccessToken, nowPlayingDataSelector } from "@/lib/spotify";
 import { GetNowPlayingResponse } from "@/types";
 
 const SPOTIFY_NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+
+export const revalidate = 10;
+
 export async function GET() {
     const { access_token: accessToken } = await getAccessToken();
     const response = await fetcher<GetNowPlayingResponse>(SPOTIFY_NOW_PLAYING_ENDPOINT, {
         headers: {
             Authorization: `Bearer ${accessToken}`
-        },
-        cache: "no-cache"
+        }
     }).catch(() => null);
 
     const data = response?.is_playing ? nowPlayingDataSelector(response) : null;
