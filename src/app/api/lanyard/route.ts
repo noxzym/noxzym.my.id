5c8f332df60fd5cdf169a0b2cb653fe97d263f3d
noxzym <orchitiadi@gmail.com>
2023-12-19 11:19:55 +0700

    chore(api/lanyard): add revalidate

--- src/app/api/lanyard/route.ts
+++ src/app/api/lanyard/route.ts
@@ -1,5 +1,7 @@
 import { fetcher } from "@/lib/fetcher";
 
+export const revalidate = 5;
+
 export async function GET() {
     const lanyard = await fetcher(
         `https://api.lanyard.rest/v1/users/${process.env.NEXT_PUBLIC_DISCORD_USER_ID}`
