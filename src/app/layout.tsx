bfcbcfc15cd5e9d2dcc24a592800ac80ec68ff2c
noxzym <orchitiadi@gmail.com>
2024-01-31 19:27:27 +0700

    chore(style): add montserrat font for heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    variable: "--font-montserrat"
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -16,7 +21,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={cn(inter.className, montserrat.variable)}>
+                {children}
+            </body>
         </html>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
             }
         },
         extend: {
+            fontFamily: {
+                heading: ["var(--font-montserrat)"]
+            },
             colors: {
                 border: "hsl(var(--border))",
                 input: "hsl(var(--input))",
