77e2a1d26632c2e8202436cdb8be775baf93df7e
noxzym <orchitiadi@gmail.com>
2024-05-24 15:50:26 +0700

    refactor: grouping pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
     return (
         <main className="flex min-h-screen items-center justify-center">
             <p className="text-6xl font-black">Hello World</p>
