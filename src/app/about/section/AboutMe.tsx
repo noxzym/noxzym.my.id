b36fd03e1d69ad35e3c4717b71c019dac0effdea
noxzym <orchitiadi@gmail.com>
2024-05-25 10:44:16 +0700

    feat(/about): add description section

---
+++ src/app/about/section/AboutMe.tsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+    return (
+        <section className="flex flex-col gap-4">
+            <p className="text-5xl font-bold">About me</p>
+            <div className="flex flex-col gap-3 text-balance">
+                <p className="text-2xl font-medium">
+                    Hi, I&apos;m Orchit. a tech enthusiast fueled by the latest advancements and
+                    eager to contribute to the ever-evolving field.
+                </p>
+                <p className="text-2xl font-medium">
+                    My expertise lies in both back-end and front-end development, wielding tools
+                    like Node.js, Golang, Typescript, Next.js, Tailwind CSS, and more to build
+                    impactful applications.
+                </p>
+                <p className="text-2xl font-medium">
+                    Fueled by a desire to solve problems and make a difference, I&apos;m constantly
+                    exploring new technologies and collaborating with inspiring teams to bring
+                    innovative ideas to life.
+                </p>
+                <p className="text-2xl font-medium">
+                    This website showcases some of my creations, and I&apos;m always open to
+                    discussing exciting new projects!
+                </p>
+            </div>
+        </section>
+    );
+}
