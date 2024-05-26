89c654bf2c668cd988905bb51cf606f1c08920cc
noxzym <orchitiadi@gmail.com>
2024-05-26 08:10:34 +0700

    chore(ThemeProvider): import dynamic to avoid hydration warnings

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import dynamic from "next/dynamic";
 import type { ThemeProviderProps } from "next-themes/dist/types";
+
+const NextThemesProvider = dynamic(async () => (await import("next-themes")).ThemeProvider, {
+    ssr: false
+});
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
