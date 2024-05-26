16be938e30bb2f79da736287a91bb14760afbba4
noxzym <orchitiadi@gmail.com>
2024-05-26 08:11:11 +0700

    chore(layout): remove 'suppressHydrationWarning'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en">
             <body
                 className={cn(
                     inter.className,
