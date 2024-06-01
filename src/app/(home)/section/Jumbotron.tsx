b3f6ce0929f85fc1476f2f0b38f7b49cdb44d042
noxzym <orchitiadi@gmail.com>
2024-06-01 19:09:01 +0700

    chore(/): responsivable

--- src/app/(home)/section/Jumbotron.tsx
+++ src/app/(home)/section/Jumbotron.tsx
@@ -1,23 +1,13 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { SiGithub, SiGoogledocs, SiLinkedin } from "react-icons/si";
 import { Button } from "@/components/ui/button";
 
 export default function Jumbotron() {
-    const [offsetHeight, setOffsetHeight] = useState<number>(0);
-    const ref = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (ref.current) {
-            setOffsetHeight(ref.current.offsetHeight);
-        }
-    }, []);
-
     return (
-        <section className="items-center justify-between py-12">
-            <div ref={ref} className="flex flex-col gap-4">
+        <section className="flex flex-col items-center justify-between py-12 md:flex-row">
+            <div className="flex flex-col gap-4">
                 <div>
                     <p className="text-xl font-semibold md:text-4xl">Hello, I am</p>
                     <p className="text-4xl font-bold md:text-6xl">Orchitiadi Ismaulana Putra</p>
@@ -69,15 +59,8 @@ export default function Jumbotron() {
                     </Button>
                 </div>
             </div>
-            {ref.current && (
-                <span
-                    className="rounded-2xl bg-foreground/5"
-                    style={{
-                        height: `${offsetHeight}px`,
-                        width: `${offsetHeight}px`
-                    }}
-                />
-            )}
+            <span className="aspect-square h-full w-auto bg-red-600" />
+            <span className="order-first mb-20 h-56 w-56 rounded-2xl bg-secondary md:order-last md:mb-0" />
         </section>
     );
 }
