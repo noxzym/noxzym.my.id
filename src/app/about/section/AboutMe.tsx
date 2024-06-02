24b3de667dcab0d9eefc1862e13381aedfc11a76
noxzym <orchitiadi@gmail.com>
2024-06-02 13:47:28 +0700

    chore(/about): add avatar placeholder

--- src/app/about/section/AboutMe.tsx
+++ src/app/about/section/AboutMe.tsx
@@ -2,25 +2,28 @@ export default function AboutMe() {
     return (
         <section className="flex flex-col gap-4 py-12 md:gap-5">
             <p className="text-4xl font-bold md:text-6xl">About me</p>
-            <div className="flex flex-col gap-3 font-medium md:w-1/2">
-                <p>
-                    Hi, I&apos;m Orchit. a tech enthusiast fueled by the latest advancements and
-                    eager to contribute to the ever-evolving field.
-                </p>
-                <p>
-                    My expertise lies in both back-end and front-end development, wielding tools
-                    like Node.js, Golang, Typescript, Next.js, Tailwind CSS, and more to build
-                    impactful applications.
-                </p>
-                <p>
-                    Fueled by a desire to solve problems and make a difference, I&apos;m constantly
-                    exploring new technologies and collaborating with inspiring teams to bring
-                    innovative ideas to life.
-                </p>
-                <p>
-                    This website showcases some of my creations, and I&apos;m always open to
-                    discussing exciting new projects!
-                </p>
+            <div className="flex justify-between">
+                <div className="flex flex-col gap-3 font-medium md:w-1/2">
+                    <p>
+                        Hi, I&apos;m Orchit. a tech enthusiast fueled by the latest advancements and
+                        eager to contribute to the ever-evolving field.
+                    </p>
+                    <p>
+                        My expertise lies in both back-end and front-end development, wielding tools
+                        like Node.js, Golang, Typescript, Next.js, Tailwind CSS, and more to build
+                        impactful applications.
+                    </p>
+                    <p>
+                        Fueled by a desire to solve problems and make a difference, I&apos;m
+                        constantly exploring new technologies and collaborating with inspiring teams
+                        to bring innovative ideas to life.
+                    </p>
+                    <p>
+                        This website showcases some of my creations, and I&apos;m always open to
+                        discussing exciting new projects!
+                    </p>
+                </div>
+                <span className="order-first mb-20 h-56 w-56 rounded-2xl bg-secondary md:order-last md:mb-0" />
             </div>
         </section>
     );
