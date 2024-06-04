ef684bf352500644aa162f3376c80f6561b44bed
noxzym <orchitiadi@gmail.com>
2024-06-04 17:48:39 +0700

    feat(utils): add generateDateFormat function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function generateDateFormat(date: Date) {
+    return date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric"
+    });
+}
