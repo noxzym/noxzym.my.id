11ea7a3e2efbf97beed7038bf24decafd86d60cc
noxzym <orchitiadi@gmail.com>
2024-06-04 18:24:24 +0700

    chore(style): customVerticalSeparator -> separator

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
         <footer className="container flex max-w-screen-xl flex-col">
             <Separator />
             <div className="grid md:grid-cols-2">
-                <div className="customVerticalSeparator flex aspect-video h-auto flex-col items-center justify-center gap-2">
+                <div className="separator flex aspect-video h-auto flex-col items-center justify-center gap-2">
                     <p className="text-4xl font-bold">Orchit.</p>
-                    <p className="text-sm font-bold text-foreground/85">
+                    <p className="text-foreground/85 text-sm font-bold">
                         Depok, West Java, Indonesia.
                     </p>
                 </div>
@@ -67,7 +67,7 @@ export default function Footer() {
             </div>
             <Separator />
             <div className="mx-auto py-6">
-                <p className="text-sm font-bold text-foreground/85">
+                <p className="text-foreground/85 text-sm font-bold">
                     Copyright &copy; 2024 Noxzym.
                 </p>
             </div>
