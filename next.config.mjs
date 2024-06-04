5e0fab5c68b25175fac4051c182d82850c24f934
noxzym <orchitiadi@gmail.com>
2024-06-04 18:25:34 +0700

    chore(nextjs): add redirects path handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/discord",
+                destination: "https://discord.com/users/243728573624614912",
+                permanent: true
+            },
+            {
+                source: "/github",
+                destination: "https://github.com/noxzym",
+                permanent: true
+            },
+            {
+                source: "/instagram",
+                destination: "https://instagram.com/orchit07",
+                permanent: true
+            },
+            {
+                source: "/linkedin",
+                destination: "https://linkedin.com/in/orchit",
+                permanent: true
+            },
+            {
+                source: "/resume",
+                destination:
+                    "https://drive.google.com/file/d/1_rRlvCXkUKSGcZKoeqj_C9_Hy99H2Kvr/view",
+                permanent: true
+            },
+            {
+                source: "/spotify",
+                destination: "https://open.spotify.com/user/21kwa7qnjne3q3w3l3x2jnahi",
+                permanent: true
+            },
+            {
+                source: "/twitter",
+                destination: "https://twitter.com/noxzym",
+                permanent: true
+            }
+        ];
+    }
+};
 
 export default nextConfig;
