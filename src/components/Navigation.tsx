5cadf17b685259c163210e2baa77da9e81482c5d
noxzym <orchitiadi@gmail.com>
2024-06-04 15:55:58 +0700

    feat(Navigation): add mobile navigation and toggle theme button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,40 @@
+"use client";
+
 import Link from "next/link";
-import { FaLeaf, FaRocket, FaUser } from "react-icons/fa";
+import { useTheme } from "next-themes";
+import { FaLeaf, FaPaintBrush, FaRocket, FaUser } from "react-icons/fa";
 import { FaHouse } from "react-icons/fa6";
 import { Button } from "./ui/button";
 
 export function TopStickyNavigation() {
+    const { resolvedTheme, setTheme } = useTheme();
     return (
-        <nav className="sticky top-0 z-50 bg-background/70 py-5 backdrop-blur-xl">
+        <nav className="bg-background/70 sticky top-0 z-50 py-5 backdrop-blur-xl">
             <div className="container flex max-w-screen-xl items-center justify-between">
-                <Button asChild variant="ghost" className="text-2xl font-bold">
+                <Button asChild variant="link" className="text-2xl font-bold">
                     <Link href="/">Orchit.</Link>
                 </Button>
-                <div className="hidden gap-4 md:flex">
-                    <Button asChild size="sm" variant="ghost" className="font-semibold">
-                        <Link href="/">Home</Link>
-                    </Button>
-                    <Button asChild size="sm" variant="ghost" className="font-semibold">
-                        <Link href="/about">About</Link>
-                    </Button>
-                    <Button asChild size="sm" variant="ghost" className="font-semibold">
-                        <Link href="/articles">Articles</Link>
-                    </Button>
-                    <Button asChild size="sm" variant="ghost" className="font-semibold">
-                        <Link href="/projects">Projects</Link>
+                <div className="flex gap-4">
+                    <div className="hidden gap-4 md:flex">
+                        <Button asChild size="sm" variant="link" className="font-semibold">
+                            <Link href="/">Home</Link>
+                        </Button>
+                        <Button asChild size="sm" variant="link" className="font-semibold">
+                            <Link href="/about">About</Link>
+                        </Button>
+                        <Button asChild size="sm" variant="link" className="font-semibold">
+                            <Link href="/articles">Articles</Link>
+                        </Button>
+                        <Button asChild size="sm" variant="link" className="font-semibold">
+                            <Link href="/projects">Projects</Link>
+                        </Button>
+                    </div>
+                    <Button
+                        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+                        size="icon"
+                        variant="secondary"
+                    >
+                        <FaPaintBrush size="20px" />
                     </Button>
                 </div>
             </div>
@@ -31,8 +44,8 @@ export function TopStickyNavigation() {
 
 export function BottomStickyNavigation() {
     return (
-        <nav className="sticky bottom-5 z-50 mx-8 mt-5 rounded-xl bg-secondary/70 backdrop-blur-xl md:hidden">
-            <div className="flex items-center justify-center text-foreground/85">
+        <nav className="bg-secondary/70 sticky bottom-5 z-50 mx-8 mt-5 rounded-xl backdrop-blur-xl md:hidden">
+            <div className="text-foreground/85 flex items-center justify-center">
                 <Button asChild variant="ghost" className="flex-grow">
                     <Link href="/">
                         <FaHouse size="20px" />
