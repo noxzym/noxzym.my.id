834d1a90d0797ee57aec70dbef202c70d56b33b0
noxzym <orchitiadi@gmail.com>
2024-06-16 11:29:11 +0700

    chore(/): add revalidate page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Articles from "./section/Articles";
 import Jumbotron from "./section/Jumbotron";
 import Projects from "./section/Projects";
 
+export const revalidate = 60;
+
 export default function HomePage() {
     return (
         <>
