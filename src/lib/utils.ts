84ef416c13b77dc5d478a10616a292e3eae84ee7
noxzym <orchitiadi@gmail.com>
2024-06-16 11:25:14 +0700

    chore(utils): add parseFrontmatter function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { IMetadata } from "@/types";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -11,4 +12,22 @@ export function generateDateFormat(date: Date) {
         month: "short",
         day: "numeric"
     });
+}
+
+export function parseFrontmatter(fileContent: string) {
+    let frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
+    let match = frontmatterRegex.exec(fileContent);
+    let frontMatterBlock = match![1];
+    let content = fileContent.replace(frontmatterRegex, "").trim();
+    let frontMatterLines = frontMatterBlock.trim().split("\n");
+    let metadata: Partial<IMetadata> = {};
+
+    frontMatterLines.forEach(line => {
+        let [key, ...valueArr] = line.split(": ");
+        let value = valueArr.join(": ").trim();
+        value = value.replace(/^['"](.*)['"]$/, "$1");
+        Object.assign(metadata, { [key.trim()]: value });
+    });
+
+    return { metadata, content };
 }
