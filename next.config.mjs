cde81b42e92c60898dedda39bfdaad9aca6c87a9
noxzym <orchitiadi@gmail.com>
2024-06-16 11:26:37 +0700

    chore(nextjs): add image remote hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "**.vercel-storage.com"
+            }
+        ]
+    },
     async redirects() {
         return [
             {
