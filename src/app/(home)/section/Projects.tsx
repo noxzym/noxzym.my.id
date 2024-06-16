ccab4f20a38ecad9c178516a05fee880d4154f83
noxzym <orchitiadi@gmail.com>
2024-06-16 11:28:55 +0700

    chore: use new card component & integrated it to blobs

--- src/app/(home)/section/Projects.tsx
+++ src/app/(home)/section/Projects.tsx
@@ -1,16 +1,31 @@
-import APLayout from "@/components/APLayout";
+import { IProject } from "@/types";
+import { getBlobs } from "@/lib/getBlobs";
+import ProjectCard from "@/components/ProjectCard";
 
-export default function Projects() {
+export default async function Projects() {
+    const projects = await getBlobs<IProject[]>("projects");
+
     return (
-        <APLayout
-            title="Projects"
-            description="Here's a glimpse into my project experience."
-            href="/projects"
-            items={new Array(3).fill({
-                title: "Project Title",
-                subTitle: "Apr. 20, 2024 - Role",
-                tags: ["Some Tags", "Some Tags", "Some Tags"]
-            })}
-        ></APLayout>
+        <section className="flex flex-col gap-5 py-12 md:gap-4">
+            <div className="flex flex-col">
+                <p className="text-3xl font-bold">Projects</p>
+                <p className="font-medium text-foreground/85">
+                    Here&apos;s a glimpse into my project experience.
+                </p>
+            </div>
+            <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+                {projects
+                    .sort((a, b) => {
+                        const dateA = new Date(a.metadata.date);
+                        const dateB = new Date(b.metadata.date);
+
+                        return dateB.getTime() - dateA.getTime();
+                    })
+                    .slice(0, 3)
+                    .map((item, i) => (
+                        <ProjectCard key={i} project={item} />
+                    ))}
+            </div>
+        </section>
     );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,34 @@
-import APLayout from "@/components/APLayout";
+import { IProject } from "@/types";
+import { getBlobs } from "@/lib/getBlobs";
+import { Input } from "@/components/ui/input";
+import ProjectCard from "@/components/ProjectCard";
 
-export default function ProjectsPage() {
+export const revalidate = 60;
+
+export default async function ProjectsPage() {
+    const projects = await getBlobs<IProject[]>("projects");
+
     return (
-        <APLayout
-            title="Projects"
-            description="Here's a glimpse into my project experience."
-            href="/projects"
-            items={new Array(3).fill({
-                title: "Project Title",
-                subTitle: "Apr. 20, 2024 - Role",
-                tags: ["Some Tags", "Some Tags", "Some Tags"]
-            })}
-        />
+        <section className="flex flex-col gap-5 py-12 md:gap-4">
+            <div className="flex flex-col">
+                <p className="text-4xl font-bold md:text-6xl">Projects</p>
+                <p className="font-medium text-foreground/85">
+                    Here&apos;s a glimpse into my project experience.
+                </p>
+            </div>
+            <Input placeholder={`Search Projects...`} />
+            <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+                {projects
+                    .sort((a, b) => {
+                        const dateA = new Date(a.metadata.date!);
+                        const dateB = new Date(b.metadata.date!);
+
+                        return dateB.getTime() - dateA.getTime();
+                    })
+                    .map((item, i) => (
+                        <ProjectCard key={i} project={item} />
+                    ))}
+            </div>
+        </section>
     );
 }
