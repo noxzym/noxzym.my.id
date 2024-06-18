f743b4bfea31aebef44d8dcbc39e67912c1e1eef
noxzym <orchitiadi@gmail.com>
2024-06-18 13:24:54 +0700

    feat(components): add MDXRemote component

---
+++ src/components/MDXRemote.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IProject } from "@/types";
+import { MDXRemote as MDXRemotePrimitive, MDXRemoteProps } from "next-mdx-remote/rsc";
+import remarkGfm from "remark-gfm";
+
+interface props {
+    project: IProject;
+}
+
+export default function MDXRemote({ project }: props) {
+    const props: MDXRemoteProps = {
+        source: project.content,
+        components: {
+            a: props => <Link href={props.href!} target="_blank" {...props} />,
+            Cover: _ => (
+                <Image
+                    src={project.metadata.image!}
+                    alt={project.metadata.title}
+                    sizes="100vw"
+                    width={400}
+                    height={225}
+                    priority
+                    className="border-1 my-0 aspect-video h-auto w-full rounded-xl"
+                />
+            )
+        },
+        options: {
+            mdxOptions: {
+                remarkPlugins: [remarkGfm]
+            }
+        }
+    };
+
+    return (
+        <div className="prose prose-zinc max-w-none dark:prose-invert">
+            <MDXRemotePrimitive {...props} />
+        </div>
+    );
+}
