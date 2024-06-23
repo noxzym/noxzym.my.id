310c61a3175f6bf9057a924167207d8865f5e682
noxzym <orchitiadi@gmail.com>
2024-06-23 17:14:08 +0700

    feat(components): add ProjectsSection component

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,35 @@
+import { IProject } from "@/types";
+import APCard from "./APCard";
+import APLayout from "./APLayout";
+import MDXRemote from "./MDXRemote";
+import ResponsiveDialog from "./ResponsiveDialog";
+
+interface props {
+    projects: IProject[];
+}
+
+export default async function ProjectsSection({ projects }: props) {
+    return (
+        <APLayout
+            title="Projects"
+            description="Here's a glimpse into my project experience."
+            href="/projects"
+            items={projects
+                .sort((a, b) => {
+                    const dateA = new Date(a.metadata.date);
+                    const dateB = new Date(b.metadata.date);
+
+                    return dateB.getTime() - dateA.getTime();
+                })
+                .map((item, i) => (
+                    <ResponsiveDialog<IProject>
+                        key={i}
+                        obj={item as IProject}
+                        trigger={<APCard data={item} />}
+                    >
+                        <MDXRemote mdx={item} />
+                    </ResponsiveDialog>
+                ))}
+        />
+    );
+}
