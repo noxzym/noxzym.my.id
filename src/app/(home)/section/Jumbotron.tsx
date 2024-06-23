eaf72a358c40e73b10d04b5d6fce9e3a0c3212a9
noxzym <orchitiadi@gmail.com>
2024-06-23 17:12:09 +0700

    chore: merge changes

--- src/app/(home)/section/Jumbotron.tsx
+++ src/app/(home)/section/Jumbotron.tsx
@@ -1,35 +1,11 @@
 import Link from "next/link";
-import { IGithub, ILanyard } from "@/types";
-import { ListBlobResultBlob } from "@vercel/blob";
 import { ExternalLinks, NavigationLinks } from "@/lib/constants";
-import { getBlobs } from "@/lib/getBlobs";
+import getProfilePicture from "@/lib/getProfilePicture";
 import { Button } from "@/components/ui/button";
 import CarouselProfilePicture from "@/components/CarouselProfilePicture";
 
 export default async function Jumbotron() {
-    const blobs = await getBlobs<ListBlobResultBlob[]>("images", "images/profile.png");
-    const profilePictureBlob = blobs[0] as unknown as ListBlobResultBlob | undefined;
-    const profilePicture = profilePictureBlob?.url ?? null;
-
-    const lanyardRequest = await fetch(
-        "https://api.lanyard.rest/v1/users/243728573624614912"
-    ).catch(() => null);
-
-    const lanyardData: ILanyard | null = lanyardRequest?.ok
-        ? await lanyardRequest?.json().catch(() => null)
-        : null;
-
-    const discordAvatar = lanyardData?.success
-        ? `https://cdn.discordapp.com/avatars/243728573624614912/${lanyardData?.data.discord_user.avatar}.png?size=512`
-        : null;
-
-    const githubResult = await fetch("https://api.github.com/users/noxzym").catch(() => null);
-
-    const githubData: IGithub | null = githubResult?.ok
-        ? await githubResult?.json().catch(() => null)
-        : null;
-
-    const githubAvatar = githubData?.avatar_url ?? null;
+    const [profilePicture, githubAvatar, discordAvatar] = await getProfilePicture();
 
     return (
         <section className="flex flex-col items-center justify-between py-12 md:mt-12 md:flex-row">
--- src/app/about/section/AboutMe.tsx
+++ src/app/about/section/AboutMe.tsx
@@ -1,32 +1,8 @@
-import { IGithub, ILanyard } from "@/types";
-import { ListBlobResultBlob } from "@vercel/blob";
-import { getBlobs } from "@/lib/getBlobs";
+import getProfilePicture from "@/lib/getProfilePicture";
 import CarouselProfilePicture from "@/components/CarouselProfilePicture";
 
 export default async function AboutMe() {
-    const blobs = await getBlobs<ListBlobResultBlob[]>("images", "images/profile.png");
-    const profilePictureBlob = blobs[0] as unknown as ListBlobResultBlob | undefined;
-    const profilePicture = profilePictureBlob?.url ?? null;
-
-    const lanyardRequest = await fetch(
-        "https://api.lanyard.rest/v1/users/243728573624614912"
-    ).catch(() => null);
-
-    const lanyardData: ILanyard | null = lanyardRequest?.ok
-        ? await lanyardRequest?.json().catch(() => null)
-        : null;
-
-    const discordAvatar = lanyardData?.success
-        ? `https://cdn.discordapp.com/avatars/243728573624614912/${lanyardData?.data.discord_user.avatar}.png?size=512`
-        : null;
-
-    const githubResult = await fetch("https://api.github.com/users/noxzym").catch(() => null);
-
-    const githubData: IGithub | null = githubResult?.ok
-        ? await githubResult?.json().catch(() => null)
-        : null;
-
-    const githubAvatar = githubData?.avatar_url ?? null;
+    const [profilePicture, githubAvatar, discordAvatar] = await getProfilePicture();
 
     return (
         <section className="flex flex-col gap-4 py-12 md:gap-5">
