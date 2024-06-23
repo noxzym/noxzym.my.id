6d1c10ab11b3ae1d84fcf928d6d25d4568e4ef3b
noxzym <orchitiadi@gmail.com>
2024-06-23 17:13:55 +0700

    feat(components): add ArticlesSection component

---
+++ src/components/ArticlesSection.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { IArticle } from "@/types";
+import APCard from "./APCard";
+import APLayout from "./APLayout";
+
+interface props {
+    articles: IArticle[];
+}
+
+export default async function ArticlesSection({ articles }: props) {
+    return (
+        <APLayout
+            title="Articles"
+            description="The following are some of the articles I have written on."
+            href="/blog"
+            items={articles
+                .sort((a, b) => {
+                    const dateA = new Date(a.metadata.date);
+                    const dateB = new Date(b.metadata.date);
+
+                    return dateB.getTime() - dateA.getTime();
+                })
+                .map((item, i) => (
+                    <Link
+                        key={i}
+                        href={`/blog/${item.metadata.title.toLowerCase().split(" ").join("-")}`}
+                    >
+                        <APCard data={item} />
+                    </Link>
+                ))}
+        />
+    );
+}
