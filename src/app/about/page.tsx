235c45978147700c7f08d23c5335dfd52ca8041b
noxzym <orchitiadi@gmail.com>
2024-06-23 17:17:44 +0700

    chore(/about): add revalidate

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,8 @@ import AboutMe from "./section/AboutMe";
 import MyCertificates from "./section/MyCertificates";
 import MyEducation from "./section/MyEducation";
 
+export const revalidate = 60;
+
 export default function AboutPage() {
     return (
         <>
