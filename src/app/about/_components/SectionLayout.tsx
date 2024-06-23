1bd1a3b2b264d548da68ecdabf98a2b51986ca6b
noxzym <orchitiadi@gmail.com>
2024-06-23 17:17:32 +0700

    chore(/about): update styling

--- src/app/about/_components/SectionLayout.tsx
+++ src/app/about/_components/SectionLayout.tsx
@@ -1,25 +1,23 @@
-export default function SectionLayout({
-    title,
-    description,
-    items
-}: {
+interface props {
     title: string;
     description: string;
     items: IItem[];
-}) {
+}
+
+export default function SectionLayout({ title, description, items }: props) {
     return (
         <section className="flex flex-col gap-4 py-12 md:gap-5">
             <div className="flex flex-col">
                 <p className="text-3xl font-bold">{title}</p>
-                <p className="text-xl font-medium text-foreground/85">{description}</p>
+                <p className="font-medium text-foreground/85">{description}</p>
             </div>
             <div className="flex flex-col gap-3 md:gap-4">
                 {items.map((item, index) => (
                     <div key={index} className="flex flex-col justify-between md:flex-row">
                         <div className="flex md:w-1/2">
-                            <span className="mx-5 mt-1 hidden h-5 w-5 flex-none rounded-full bg-foreground md:block" />
+                            <span className="mx-5 mt-1 hidden h-4 w-4 flex-none rounded-full bg-foreground md:block" />
                             <div className="flex flex-col">
-                                <p className="line-clamp-1 text-2xl font-medium">{item.title}</p>
+                                <p className="line-clamp-1 text-xl font-medium">{item.title}</p>
                                 <p className="font-medium text-foreground/85">{item.provider}</p>
                             </div>
                         </div>
