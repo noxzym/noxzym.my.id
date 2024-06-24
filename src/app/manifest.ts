a052cf6ff73f63084bddf1f21f0049ee48f622e1
noxzym <orchitiadi@gmail.com>
2024-06-24 11:30:59 +0700

    feat(app): add manifest generator

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+import { DefaultMetadata } from "@/lib/constants";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        id: "/",
+        name: "Noxzym",
+        short_name: "Noxzym",
+        theme_color: "#09090b",
+        background_color: "#09090b",
+        description: DefaultMetadata.description,
+        display: "standalone",
+        orientation: "portrait",
+        display_override: ["window-controls-overlay"],
+        scope: "/",
+        start_url: "/",
+        icons: DefaultMetadata.icons
+            .filter(icon => Number(icon.width!) >= 192)
+            .map(icon => ({
+                src: icon.url.toString(),
+                sizes: icon.sizes,
+                type: icon.type
+            }))
+    };
+}
