8c6fcee2aa982775836b181792410dfd049aa263
noxzym <orchitiadi@gmail.com>
2024-08-03 08:46:44 +0700

    feat(PWA): add service worker for pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+    // Note: This is only an example. If you use Pages Router,
+    // use something else that works, such as "service-worker/index.ts".
+    swSrc: "src/app/sw.ts",
+    swDest: "public/sw.js"
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["next-mdx-remote"],
@@ -50,4 +59,4 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
